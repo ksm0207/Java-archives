8d90d6fa0b80da90aee90a551ea610f517afb9d2
ksm0207 <ksm03071@naver.com>
2021-02-26 03:02:19 +0900

    Part 01-1 순차적인 프로그램 작성하기
    - 블로그 포스팅 전용 프로그램

---
+++ src/com/company/Hello.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("배가고프다");
+        System.out.println("하지만 참아야 된다");
+        System.out.println("왜냐면 지금 먹으면 살찌니깐..");
+
+    }
+}
