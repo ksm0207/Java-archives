d4412477527dca280498ae60e3dca97582696b77
ksm0207 <ksm03071@naver.com>
2021-03-03 08:30:56 +0900

    Test

---
+++ src/com/company/VariableType.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class VariableType {
+    public static void main(String [] args){
+
+        int a = 20;
+        int b = 020;
+        int c = 0x20;
+        int d = 0b0101;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
--- src/com/company/variable.java
+++ src/com/company/variable.java
@@ -4,8 +4,6 @@ public class variable {
 
     public static void main(String [] args){
         
-        int number = 10;
-        System.out.println("number 는 " + number + "입니다");
 
     }
 
