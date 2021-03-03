c22a124c39698d26d8b6f12e7c07ff8bf759f192
ksm0207 <ksm03071@naver.com>
2021-03-03 10:44:09 +0900

    Part 01-5 String 클래스 01

---
+++ src/expression/StringTest.java
@@ -0,0 +1,11 @@
+package expression;
+
+public class StringTest {
+    public static void main(String [] args){
+
+        String str = "이것은 String 타입 객체 입니다";
+        String str2 = "lalalalala";
+
+        System.out.println(str +"\n" +str2);
+    }
+}
