fb7d6670f786dddfc709427d01384a814482a63d
ksm0207 <ksm03071@naver.com>
2021-03-05 12:23:52 +0900

    Part 2 Example
    제어문 if - else

---
+++ src/part02/Larger.java
@@ -0,0 +1,34 @@
+package part02;
+
+import java.util.Scanner;
+
+public class Larger {
+    public static void main(String [] args){
+
+        int f_value =0;
+        int s_value =0;
+        int result = 0;
+        String str = "";
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("첫번째 정수를 입력하세요 : ");
+        f_value = scan.nextInt();
+        System.out.println("두번째 정수를 입력하세요 : ");
+        s_value = scan.nextInt();
+
+        result = f_value;
+        System.out.println(result);
+
+        result = s_value;
+        System.out.println(result);
+
+        if ( f_value > s_value){
+            result = f_value;
+            str = "첫번째";
+        }else{
+            result = s_value;
+            str = "두번째";
+        }
+        System.out.println("가장 큰 수는 " + result + " 입니다 "+ str + " 정수가 크네요");
+    }
+}
