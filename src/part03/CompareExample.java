521ea65fbde5197a9c13618a4ca2623afdfe0278
ksm0207 <ksm03071@naver.com>
2021-03-05 21:54:25 +0900

    백준 알고리즘 Update

---
+++ src/part03/CompareExample.java
@@ -0,0 +1,22 @@
+package part03;
+
+import java.util.Scanner;
+
+public class CompareExample {
+    public static void main(String [] args){
+
+        int f_value = 0;
+        int s_value = 0;
+        Scanner scan = new Scanner(System.in);
+        f_value = scan.nextInt();
+        s_value = scan.nextInt();
+
+        if (f_value < s_value){
+            System.out.println("<");
+        }else if (f_value == s_value){
+            System.out.println("==");
+        }else{
+            System.out.println(">");
+        }
+    }
+}
---
+++ src/part03/ScoreExample.java
@@ -0,0 +1,35 @@
+package part03;
+
+import java.util.Scanner;
+
+public class ScoreExample {
+    public static void main(String [] args){
+
+        int f_value = 0;
+        int result = 0;
+        String grade = "";
+
+        Scanner scan = new Scanner(System.in);
+        f_value = scan.nextInt();
+        result = f_value / 10;
+
+        switch (result){
+            case 10:
+            case 9:
+                grade ="A";
+                break;
+            case 8:
+                grade ="B";
+                break;
+            case 7:
+                grade ="C";
+                break;
+            case 6:
+                grade ="D";
+                break;
+            default:
+                grade = "F";
+        }
+        System.out.println(grade);
+    }
+}
