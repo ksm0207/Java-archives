3f7804f4ecbb0722abe0e965f0f841465b128cbf
ksm0207 <ksm03071@naver.com>
2021-03-05 17:37:02 +0900

    Part 2 switch
    - switch 문 예제

---
+++ src/part02/ScoreExample.java
@@ -0,0 +1,40 @@
+package part02;
+
+import java.util.Scanner;
+
+public class ScoreExample {
+    public static void main(String [] args){
+
+        int score = 0; // 학점
+        int result = 0; // 합
+        String grade = ""; // 등급
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("자신의 학점을 입력하십시오 : ");
+        score = scan.nextInt();
+        result = score / 10;
+
+        switch (result){
+            case 10:
+            case 9:
+                 grade="A+";
+                 break;
+            case 8:
+                 grade="A";
+                 break;
+            case 7:
+                 grade="B+";
+                 break;
+            case 6:
+                 grade="B";
+                 break;
+            case 5:
+                 grade="C";
+                 break;
+            default:
+                grade="F";
+                break;
+        }
+        System.out.println("학점 결과 : " + grade +  " 입니다 ");
+    }
+}
