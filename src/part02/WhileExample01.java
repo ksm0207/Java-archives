eb1adb63c93265b713e09cd4ba72e784e7b577fd
ksm0207 <ksm03071@naver.com>
2021-03-07 18:25:01 +0900

    Part 2 while문 Example
    - Example commit

---
+++ src/part02/WhileExample01.java
@@ -0,0 +1,20 @@
+package part02;
+
+import java.util.Scanner;
+
+public class WhileExample01 {
+    public static void main(String [] args){
+
+        int number =0;
+        int default_value = 0;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("입력한 값 까지 정수 값 입력 하기 : ");
+        number = scan.nextInt();
+
+        while (default_value < number){
+            System.out.println(default_value);
+            default_value ++;
+        }
+    }
+}
---
+++ src/part02/WhileTest.java
@@ -0,0 +1,13 @@
+package part02;
+
+public class WhileTest {
+    public static void main(String [] args){
+
+        int number = 0;
+
+        while (number < 5){
+            System.out.println("while 문이" + number + "번 실행 하였습니다 ");
+        }
+        System.out.println("프로그램 종료.");
+    }
+}
