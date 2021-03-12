d8f785c90ec5c08df717d63364fb8ccd98cda381
ksm0207 <ksm03071@naver.com>
2021-03-12 22:16:38 +0900

    Part 03 배열 예제풀이
    -

---
+++ src/part03/ArrayTest03.java
@@ -0,0 +1,12 @@
+package part03;
+
+public class ArrayTest03 {
+    public static void main (String [] ags){
+
+        int [] start = {10 , 20 , 30};
+
+        for ( int i = 0 ; i < start.length ; i ++){
+            System.out.println(start[i]);
+        }
+    }
+}
---
+++ src/part03/PizzaTopping.java
@@ -0,0 +1,12 @@
+package part03;
+
+
+public class PizzaTopping {
+    public static void main ( String [] args) {
+
+        String [] topping = {"올리브","페퍼로니","감자","고구마","버섯"};
+        for (int i = 0 ; i < topping.length ; i ++) {
+            System.out.println(topping[i]);
+        }
+    }
+}
