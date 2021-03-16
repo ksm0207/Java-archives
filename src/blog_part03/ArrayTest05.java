9815a7d2d54d93877b3c68b51de93ba63bb82533
ksm0207 <ksm03071@naver.com>
2021-03-17 00:26:40 +0900

    blog_part03 - 예제

---
+++ src/blog_part03/ArrayTest05.java
@@ -0,0 +1,19 @@
+package blog_part03;
+
+public class ArrayTest05 {
+    public static void main(String [] args){
+
+        System.out.println(sum(new int [] {10,20,30,40,50}));
+
+    }
+    public static int sum ( int [] arguments){
+
+        int result = 0;
+
+        for (int i = 0 ; i < arguments.length ; i ++) {
+            result = result + arguments[i];
+        }
+        return result;
+    }
+
+}
