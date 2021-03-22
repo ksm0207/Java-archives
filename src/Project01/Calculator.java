0df9cdad909951e7193df2d912a7ddbf6593cfa1
ksm0207 <ksm03071@naver.com>
2021-03-23 04:30:08 +0900

    계산기 Update
    - 문자열 입력시 예외발생
    - 곱셈 나눗셈 기능 추가

--- src/Project01/Calculator.java
+++ src/Project01/Calculator.java
@@ -6,18 +6,20 @@ public class Calculator {
     public static void main ( String [] args){
 
         Scanner scan = new Scanner(System.in);
-        String [] menu = new String[] {"0 : 종료" , "1 : 덧셈" , "2 : 뺄셈"};
-        int frist = 0;
-        int second = 0;
+        String [] menu = new String[] {"0 : 종료" , "1 : 덧셈" , "2 : 뺄셈" , "3 : 곱셈" , "4 : 나눗셈"};
+        int frist, second ;
         int selectNumber= 0;
-        boolean is = true;
+        final int max = 1000000000;
 
         System.out.println("성민s 계산기");
+
         while (true){
             for (int i = 0 ; i < menu.length; i ++){
                 System.out.println(menu[i]);
             }
             System.out.println("메뉴를 선택하십시오.");
+
+        try {
             selectNumber = Integer.parseInt(scan.next());
 
             if (selectNumber == 1){
@@ -34,16 +36,34 @@ public class Calculator {
                 System.out.println("두번째 정수를 입력하십시오");
                 second = scan.nextInt();
                 System.out.println("결과 :" + Minus(frist,second));
-            }else{
-                if (is){
-                    is = false;
-                    System.out.println(is);
-                    System.out.println("              ______");
-                    System.out.println("시스템 종료  r-[._.]-r");
-                    System.out.println("       =))=)) ------");
-                    System.out.println("             -ㄴ---ㄴ");
+            }else if (selectNumber == 3){
+                System.out.println("곱셈 시작");
+                System.out.println("첫번째 정수를 입력하십시오");
+                frist = scan.nextInt();
+                System.out.println("두번째 정수를 입력하십시오");
+                second = scan.nextInt();
+                System.out.println("결과 :" + Multi(frist,second));
+            }else if (selectNumber == 4){
+                System.out.println("나눗셈 시작");
+                System.out.println("첫번째 정수를 입력하십시오");
+                frist = scan.nextInt();
+                System.out.println("두번째 정수를 입력하십시오");
+                second = scan.nextInt();
+                System.out.println("결과 :" + Div(frist,second));
+            }
+            else{
+                if (selectNumber == 0){
+                    System.out.println("                    ______");
+                    System.out.println("프로그램 종료     r-[._.]-r");
+                    System.out.println("             =))=)) ------");
+                    System.out.println("                  -ㄴ---ㄴ");
                     break;
+                }else if (selectNumber < max){
+                    System.out.println("0 부터 4 까지의 숫자를 입력하십시오.");
                 }
+            }
+        }catch (Exception e){
+            System.out.println("Error : " + " 특수문자 혹은 기호가 감지되었습니다 숫자만을 허용합니다 " + e);
             }
         }
     }
@@ -58,4 +78,16 @@ public class Calculator {
         int b = num2;
         return a-b;
     }
+    public static int Multi(int num , int num2){
+        int a = num;
+        int b = num2;
+
+        return a * b;
+    }
+    public static int Div(int num, int num2){
+        int a = num;
+        int b = num2;
+        return a / b;
+    }
+
 }
