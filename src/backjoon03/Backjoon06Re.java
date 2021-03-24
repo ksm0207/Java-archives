fc48ce3118e7b8fd53676dd07651be17c8b8a431
ksm0207 <ksm03071@naver.com>
2021-03-25 03:13:29 +0900

    백준 알고리즘 5단계 풀이중 (5/7) Update
    - OX 문제 완벽 풀이

---
+++ src/backjoon03/Backjoon06Re.java
@@ -0,0 +1,37 @@
+package backjoon03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Backjoon06Re {
+    public static void main(String [] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int forCount = Integer.parseInt(br.readLine());
+        int countScore ,result;
+        String [] arr = new String[forCount];
+
+        for (int i = 0 ; i < arr.length; i ++) {
+            arr[i] = String.valueOf(br.readLine());
+        }
+        for (int i = 0 ; i < forCount ; i ++) { // forCount로 발생시킨 반복문 횟수만큼 진행
+
+            // for 반복문이 실행될때마다 0으로 초기화 되면서 연산
+            countScore = 0;
+            result = 0;
+
+            for (int j = 0; j < arr[i].length(); j ++) { // arr[i].length()는 배열의 데이터 길이를 반환
+
+                // 배열에 담긴 데이터중에 O 가 있는지 체크
+                if (arr[i].charAt(j) == 'O') {
+                    // 존재하는 만큼 countScore를 증감
+                    countScore++;
+                } else {
+                    countScore = 0;
+                }
+                result = result + countScore;
+            }
+            System.out.println(result);
+        }
+    }
+}
