8c11249ae2245e9236f2c307563f836f9d083413
ksm0207 <ksm03071@naver.com>
2021-04-05 17:57:46 +0900

    백준 알고리즘 6단계 풀이중 (3/10)
    - 알파벳 찾기

---
+++ src/backjoon05/BackJoon03.java
@@ -0,0 +1,28 @@
+package backjoon05;
+
+import java.util.Scanner;
+
+public class BackJoon03 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int [] arr = new int[26];
+
+        for (int i = 0 ; i < arr.length ; i ++) {
+            arr[i] = -1;
+        }
+        // backjoon
+        String S = scan.nextLine();
+
+        for (int i = 0 ; i < S.length() ; i ++) {
+            char resValue = S.charAt(i);
+
+            if ( arr[resValue - 'a'] == -1 ) {
+                arr[resValue - 'a'] = i;
+            }
+        }
+        for (int res : arr){
+            System.out.println(res + " ");
+        }
+    }
+}
