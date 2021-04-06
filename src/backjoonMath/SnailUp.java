dbf1b22f6a0764f03cbd0506a9f4a9c0a4c30730
ksm0207 <ksm03071@naver.com>
2021-04-07 05:33:44 +0900

    `
    d

---
+++ src/backjoonMath/SnailUp.java
@@ -0,0 +1,37 @@
+package backjoonMath;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SnailUp {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int snailUp = Integer.parseInt(st.nextToken());
+        int snailDown = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int dayRes = (V - snailDown) / (snailUp - snailDown);
+
+        // 2 1 5 값을 입력했을때
+        // dayRes = ( 5 - 1 ) / ( 2 - 1) 는 4 / 1 이므로 4일이 된다 ? [확인]
+
+        // 5 1 6 값을 입력했을때
+        // dayRes = ( 6 - 1 ) / ( 5 - 1 ) 는 5 / 4 이므로 1일이 된다 ? [확인]
+        // 하지만 출력해야 할 값은 2 인데..
+
+
+        // 1. dayRes 변수가 가지는 식을 가지고 몫이     1일 경우 dayRes 증감 조건식 / [실패]
+        // 1. dayRes 변수가 가지는 식을 가지고 나머지는 1일 경우 dayRes 증감 조건식 % [실패]
+
+        if ( (V - snailDown) % (snailUp - snailDown) == 1  ) {
+            dayRes ++;
+            System.out.println(dayRes);
+        }else{
+            System.out.println(dayRes);
+        }
+    }
+}
