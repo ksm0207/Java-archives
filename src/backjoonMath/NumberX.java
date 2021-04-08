54c12641af6e6f3c9c3ec8d59ae6d85ad58ca422
ksm0207 <ksm03071@naver.com>
2021-04-08 14:39:03 +0900

    백준 10871번
    - X보다 작은 수 출력

---
+++ src/backjoonMath/NumberX.java
@@ -0,0 +1,29 @@
+package backjoonMath;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NumberX {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine()," ");
+
+        for (int i = 0 ; i < N ; i ++) {
+
+            int input = Integer.parseInt(st.nextToken());
+
+            if (input < X) {
+                sb.append(input).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
