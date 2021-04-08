bacea69cad78ad4a717f5879b06d10ced76b100a
ksm0207 <ksm03071@naver.com>
2021-04-08 20:50:08 +0900

    백준 단계별 : 기본수학1 ( 3/9 ) 풀이
    - 큰 수 입력받고 합 하기

---
+++ src/backjoonMath/BigNumber.java
@@ -0,0 +1,14 @@
+package backjoonMath;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigNumber {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        BigInteger x = scan.nextBigInteger();
+        BigInteger y = scan.nextBigInteger();
+        BigInteger sum = x.add(y);
+        System.out.println(sum);
+    }
+}
