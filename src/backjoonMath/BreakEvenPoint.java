22cca25b68a541a8600ea3a176fa2920451a06f9
ksm0207 <ksm03071@naver.com>
2021-04-08 17:08:25 +0900

    백준 단계별 : 기본수학1 ( 2/9 ) 풀이
    - 손익분기점 구하기 프로그램

---
+++ src/backjoonMath/BreakEvenPoint.java
@@ -0,0 +1,27 @@
+package backjoonMath;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BreakEvenPoint {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        // 0. 손익분기점 을 구하는 프로그램
+
+        int fixedMoney = Integer.parseInt(st.nextToken()); // 고정비용
+        int variableCost = Integer.parseInt(st.nextToken()); // 가변비용
+        int price = Integer.parseInt(st.nextToken()); // 측정 값
+
+        if (price <= variableCost) {
+            // 170 <= 70
+            // 1 <= 2
+            System.out.println("-1");
+        }else{
+            System.out.println( (fixedMoney / (price - variableCost))+1 );
+        }
+    }
+}
+
