17c5e7f3c691b9323bfd9e41a6844decda4d648c
ksm0207 <ksm03071@naver.com>
2021-04-13 04:44:12 +0900

    Java - 예외처리 (4)
    - NumberFormatException

---
+++ src/BlogPost/NumberFormatException.java
@@ -0,0 +1,27 @@
+package BlogPost;
+
+import java.util.Scanner;
+
+public class NumberFormatException {
+
+    public int parseInt(String str){
+        try {
+            System.out.println("Result : " + str);
+            return Integer.parseInt(str);
+
+        }catch (java.lang.NumberFormatException error){
+            System.out.println("Catch 발생");
+            System.out.println(error);
+            System.out.println(error.getMessage());
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        NumberFormatException nfe = new NumberFormatException();
+        Scanner scan = new Scanner(System.in);
+        String strInput = scan.next();
+        nfe.parseInt(strInput);
+
+    }
+}
