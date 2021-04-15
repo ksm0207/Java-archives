9f56d99bc2213b5d308062983062a76708f332ee
ksm0207 <ksm03071@naver.com>
2021-04-16 03:20:26 +0900

    백준 단계별 : while ( 2/3 ) 풀이완료
    - EOF 처리 ( end of file )
    - 읽을 수 없는 데이터 존재 시 반복문 종료

--- src/backjoon06/BackJoon02.java
+++ src/backjoon06/BackJoon02.java
@@ -1,7 +1,30 @@
 package backjoon06;
 
-public class BackJoon02 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class BackJoon02 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int x;
+        int y;
+
+        // 1. 0 2개 일때는 프로그램 종료 아닐때는 ?
+        while (true){
+            try {
+                st = new StringTokenizer(br.readLine()," ");
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
+                System.out.println(x + y);
+            }catch (Exception error){
+                System.out.println(error);
+                break;
+            }
+
+        }
     }
 }
