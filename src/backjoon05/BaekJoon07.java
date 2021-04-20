75f6abfe6b1f0817ea0ed236b9f6ad6bc6845cfa
ksm0207 <ksm03071@naver.com>
2021-04-21 05:28:18 +0900

    백준 알고리즘 6단계 풀이중 (6/10)
    - 2908번 상수

---
+++ src/backjoon05/BaekJoon07.java
@@ -0,0 +1,20 @@
+package backjoon05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BaekJoon07 {
+    public static void main(String[] args) throws Exception{
+        // 1. 단계 6 (Git) 문자열 - 2908번 상수 (상근이동생..)
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        // 1. 두수를 입력받기
+        int x = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int y = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println( x > y ? x : y);
+    }
+}
