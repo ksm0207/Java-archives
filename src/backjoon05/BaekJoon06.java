3e07811c90a0aa01f30e1fc0dcb5202397cfae6a
ksm0207 <ksm03071@naver.com>
2021-04-21 03:17:40 +0900

    백준 알고리즘 6단계 풀이중 (5/10)
    - 1152번 단어의 개수

--- src/backjoon05/BaekJoon06.java
+++ src/backjoon05/BaekJoon06.java
@@ -1,7 +1,27 @@
 package backjoon05;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BaekJoon06 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)  throws Exception{
+        // 1. 단계 6 (Git) 문자열 - 1152번 단어의 개수
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] S = br.readLine().split(" ");
+        int count =0;
+
+        for (int i = 0 ; i < S.length; i ++) {
+            try{
+                char find = S[i].charAt(0);
+                count ++;
+            }catch (Exception error) {
+            }
+        }
+        System.out.println(count);
     }
 }
+
+
+
+
