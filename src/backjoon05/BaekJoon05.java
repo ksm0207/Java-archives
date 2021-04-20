d7114232d823fa4c8e29d9ddc4cdab6e90ebb582
ksm0207 <ksm03071@naver.com>
2021-04-21 00:59:53 +0900

    백준 알고리즘 6단계 풀이중
    - 단어공부 풀이중

---
+++ src/backjoon05/BaekJoon05.java
@@ -0,0 +1,17 @@
+package backjoon05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon05 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        for (int i = 0 ; i < input.length(); i ++) {
+            char find = input.charAt(i);
+
+        }
+    }
+}
---
+++ src/backjoon05/BaekJoon06.java
@@ -0,0 +1,7 @@
+package backjoon05;
+
+public class BaekJoon06 {
+    public static void main(String[] args) {
+        
+    }
+}
