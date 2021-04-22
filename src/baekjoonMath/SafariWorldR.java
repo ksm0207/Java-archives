08ddae8587d5fcf4989ec2f2c53e9ced19135457
ksm0207 <ksm03071@naver.com>
2021-04-22 10:06:55 +0900

    백준 단계별 : 수학 풀이 (4) 재 풀이
    - 사파리 월드 재 풀이

--- src/baekjoonMath/SafariWorldR.java
+++ src/baekjoonMath/SafariWorldR.java
@@ -4,24 +4,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.logging.Logger;
 
-public class SafariWorld {
+public class SafariWorldR {
     public static void main(String[] args) throws IOException {
 
-        // 1. (Git) 단계별 : 수학 - 2420번 사파리월드
-
-        // ※ (|N-M|) 의 차이를 구한다 -2 5 의 차이는 7
-        // -2 1 0 1 2 3 4 5
-        //    1 2 3 4 5 6 7
+        // 1. ( | N - M | ) - 절대 값 구하는 문제..
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
         long N = Long.parseLong(st.nextToken());
         long M = Long.parseLong(st.nextToken());
-        long result = N - M;
-
-        System.out.println(Math.abs(result));
+        long abs = N - M;
+        System.out.println(Math.abs(abs));
     }
 }
