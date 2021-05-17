21716bb30c8daa1c1220b29edc8dde2dc01658ac
ksm0207 <ksm03071@naver.com>
2021-05-17 14:12:54 +0900

    백준 2743번 ( 분류 : 문자열 ) 풀이
    - 단어 길이 재기

---
+++ src/backjoon05/B2743.java
@@ -0,0 +1,17 @@
+package backjoon05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2743 {
+    public static void main(String[] args) throws IOException {
+        // Git - 백준 2743 단어 길이 재기 ( 분류 : 문자열 )
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(str.length());
+    }
+}
