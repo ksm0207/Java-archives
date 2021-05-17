ccc1851b953c1cf157311531b9b4e45857dfd42b
ksm0207 <ksm03071@naver.com>
2021-05-17 14:34:59 +0900

    백준 10808번 ( 분류 : 문자열 ) 풀이
    - 알파벳 개수 찾기

---
+++ src/backjoon05/B10808.java
@@ -0,0 +1,28 @@
+package backjoon05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10808 {
+    public static void main(String[] args) throws IOException {
+        // Git - 백준 10808 알파벳 개수 찾기 ( 분류 : 문자열 )
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+        int [] alpha = new int[26];
+
+        for (int i = 0 ; i < alpha.length; i ++) {
+            alpha[i] = 0;
+        }
+
+        for (int i = 0 ; i < S.length() ; i ++) {
+            // 알파벳이 등장할 때 마다 카운트
+            alpha[S.charAt(i)- 'a'] ++;
+        }
+        for (int res : alpha){
+            System.out.println(res);
+        }
+    }
+}
