5b00d2fcfb6abc1b1d70836d0a54c760581759c1
ksm0207 <ksm03071@naver.com>
2021-05-27 01:38:38 +0900

    백준 패키지 이름 변경 및 4999번 풀이
    - 4999번 아 !

--- src/backjoon05String/B4999.java
+++ src/backjoon05String/B4999.java
@@ -1,4 +1,23 @@
 package backjoon05String;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class B4999 {
+    public static void main(String[] args) throws IOException {
+        // Git - 백준 4999 아! ( 분류 : 문자열 )
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String F = br.readLine();
+        String S = br.readLine();
+
+        if (F.length() < S.length()) {
+            System.out.println("no");
+        }else{
+            System.out.println("go");
+        }
+    }
 }
